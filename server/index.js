f46f638996a11c4e017be0053cbe5ac66dd4371d
PatrickEBlack <g00410388@atu.ie>
2023-12-18 18:34:21 +0000

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const cors = require("cors");
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.json("Hello World");
+});
+app.get("*", (req, res) => {
+  res.sendStatus("404");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
